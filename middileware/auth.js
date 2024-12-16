802abb34ba8607f13129600be9caff8ba78f3cb9
Asif Ikbal Mondal <asikbal7ai@gmail.com>
2024-12-16 21:44:29 +0530

    added

--- middileware/auth.js
+++ middileware/auth.js
@@ -1,29 +1,6 @@
 const {getUser}=require("../service/auth");
 
-// async function restrictToLoggedinUserOnly(req,res,next){
-    
-//     // const  userUid=req.cookies?.uid;     //after req get the uid from cookie
 
-//     const userUid = req.headers["authorization"]; // Retrieve the authorization header
-//     console.log(req.headers);
-//     if(!userUid) return res.redirect("/login");
-//     const token = userUid?.split("Bearer ")[1] || req.query.token;
-//     // const User=getUser(userUid);     //that use for using cookie
-//     const User=getUser(token);
-//     if(!User) return res.redirect("/login");
-//     req.user=User;      
-//     next();
-
-// }       
-// async function checkAuth(req,res,next){
-//     // const  userUid=req.cookies?.uid; //after req get the uid from cookie
-//     const userUid = req.headers["authorization"]; // Retrieve the authorization header
-//     const token = userUid?.split("Bearer ")[1] || req.query.token;
-//     // const User=getUser(userUid); //that use for using cookie
-//     const User=getUser(token);
-//     req.user=User;
-//     next(); 
-// }       
 
 
  function checkForAuthorization(req,res,next){
